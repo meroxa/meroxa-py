b76cf51063028f32e56643d5062d6ad0fbe03d53
Eric Cheatham <eric@meroxa.io>
2022-02-25 16:19:33 -0600

    Creating a resource is complicated

--- src/meroxa/resources.py
+++ src/meroxa/resources.py
@@ -1,3 +1,10 @@
+import json
+
+from .types import CreateResourceParams
+from .types import UpdateResourceParams
+
+from .utils import ComplexEncoder
+
 RESOURCE_PATH = "/v1/resources"
 
 
@@ -17,10 +24,18 @@ class Resources:
         async with self._session.delete(RESOURCE_PATH + "/{}".format(nameOrId)) as resp:
             return await resp.text()
 
-    # Options most likely would be passed in like a dictionary
-    async def create(self, createParam: dict):
+    async def create(self, createResourceParameters: CreateResourceParams):
         async with self._session.post(
             RESOURCE_PATH,
-            data=createParam
+            data=json.dumps(createResourceParameters.reprJSON(),
+                            cls=ComplexEncoder)
+        ) as resp:
+            return await resp.text()
+
+    async def update(self, updateResourceParameters: UpdateResourceParams):
+        async with self._session.post(
+            RESOURCE_PATH + "/{}".format(updateResourceParameters.name),
+            data=json.dumps(updateResourceParameters.reprJSON(),
+                            cls=ComplexEncoder)
         ) as resp:
             return await resp.text()
--- src/meroxa/types.py
+++ src/meroxa/types.py
@@ -1,5 +1,130 @@
+from enum import Enum
+
 class ClientOptions:
     def __init__(self, auth: str, url: str, timeout=0.0):
         self.auth = auth
         self.timeout = timeout
         self.url = url
+
+
+class ResourceCredentials:
+    def __init__(self,
+                 username: str,
+                 password: str,
+                 ca_cert: str,
+                 client_cert: str,
+                 client_cert_key: str,
+                 ssl: bool):
+        self.username = username
+        self.password = password
+        self.ca_cert = ca_cert
+        self.client_cert = client_cert
+        self.client_cert_key = client_cert_key
+        self.ssl = ssl
+
+    def reprJSON(self):
+        return dict(
+            username=self.username,
+            password=self.password,
+            ca_cert=self.ca_cert,
+            client_cert=self.client_cert,
+            client_cert_key=self.client_cert_key,
+            ssl=self.ssl)
+
+
+class EnvironmentIdentifier:
+    def __init__(self, name=None, uuid=None):
+        self.name = name
+        self.uuid = uuid
+
+    def reprJSON(self):
+        return dict(
+            name = self.name,
+            uuid = self.uuid
+        )
+
+
+class ResourceMetadata:
+    def __init__(self, metadata: dict):
+        self.metadata = metadata
+    def reprJSON(self):
+        return self.metadata
+
+
+class ResourceType(Enum):
+    POSTGRES = "postgres"
+    MYSQL = "mysql"
+    URL = "url"
+    S3 = "s3"
+    MONGODB = "mongodb"
+    ELASTICSEARCH = "elasticsearch"
+    SNOWFLAKE = "snowflake"
+    BIGQUERY = "bigquery"
+    SQLSERVER = "sqlserver"
+    COSMODB = "cosmodb"
+
+
+class ResourceSSHTunnel:
+    def __init__(self, address: str, publicKey: str):
+        self.address = address
+        self.publicKey = publicKey
+
+    def reprJSON(self):
+        return dict(address=self.address, publicKey=self.publicKey)
+
+
+class CreateResourceParams:
+    def __init__(self,
+                 name: str,
+                 url: str,
+                 createCredentials: ResourceCredentials,
+                 environment: EnvironmentIdentifier,
+                 metadata: ResourceMetadata,
+                 type: ResourceType,
+                 sshTunnel: ResourceSSHTunnel):
+        self.name = name
+        self.url = url
+        self.credentials = createCredentials
+        self.environment = environment
+        self.metadata = metadata
+        self.type = type
+        self.sshTunnel = sshTunnel
+
+    def reprJSON(self):
+        return dict(
+            name=self.name,
+            url=self.url,
+            credentials=self.credentials,
+            environment=self.environment,
+            metadata=self.metadata,
+            type=self.type.value,
+            ssh_tunnel=self.sshTunnel
+        ) 
+
+class UpdateResourceParams:
+    def __init__(self,
+                 name: str,
+                 url: str,
+                 createCredentials: ResourceCredentials,
+                 environment: EnvironmentIdentifier,
+                 metadata: ResourceMetadata,
+                 type: ResourceType,
+                 sshTunnel: ResourceSSHTunnel):
+        self.name = name
+        self.url = url
+        self.credentials = createCredentials
+        self.environment = environment
+        self.metadata = metadata
+        self.type = type
+        self.sshTunnel = sshTunnel
+
+    def reprJSON(self):
+        return dict(
+            name=self.name,
+            url=self.url,
+            credentials=self.credentials,
+            environment=self.environment,
+            metadata=self.metadata,
+            type=self.type.value,
+            ssh_tunnel=self.sshTunnel
+        ) 
