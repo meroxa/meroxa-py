ef55a7ba265706ac4a1aecdacc3805a244d08353
Eric Cheatham <eric@meroxa.io>
2022-04-19 14:09:16 -0500

    prep meroxa-py for pypi

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
 import setuptools
 
+# Metadata is in setup.cfg, this is for GitHub's dependency graph
 setuptools.setup(
     name="meroxa-py",
-    version="0.0.1",
-    author="Meroxa",
-    author_email="dev@meroxa.io",
-    url="https://github.com/meroxa/meroxa-py",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    install_requires=[
+        "aiohttp >= 3.8.1"
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
 )
