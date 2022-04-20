51af0f8544abf024402a81daab6ccaf1db2e10c6
Eric Cheatham <eric@meroxa.io>
2022-04-20 11:44:10 -0500

    prefer setup.cfg over setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-# Metadata is in setup.cfg, this is for GitHub's dependency graph
-setuptools.setup(
-    name="meroxa-py",
-    install_requires=[
-        "aiohttp >= 3.8.1"
-    ],
-)
